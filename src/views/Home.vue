c521e087477be88abd58c45c627a7d5c51fc8a92
Quentin Goinaud <armaldio@gmail.com>
2019-06-26 15:13:30 +0200

    v1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <v-layout>
+            <v-flex xs9>
+                <v-select
+                        :items="releaseTag"
+                        label="Release"
+                        v-model="selectedReleaseTag"
+                ></v-select>
+            </v-flex>
+            <v-flex xs3>
+                <v-btn @click="dl">{{ state }}</v-btn>
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+            <v-flex xs6 md4 lg3 v-for="asset in selectedReleaseAssets" :key="asset.id">
+                <v-checkbox
+                        v-model="selectedFiles"
+                        :value="asset.id"
+                        :label="asset.name | formatName"></v-checkbox>
+            </v-flex>
+        </v-layout>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import ky from 'ky';
+import { saveAs } from 'file-saver';
+// (.*?)-(.*?)-(.*?)-(.*?)-(.*?)\.node
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  filters: {
+    formatName(value) {
+      return value.replace(/(.*?)-(.*)-(v.*?)-(.*?)-(.*?)\.node/, '$2 $3 $4 $5');
+    },
+  },
+  computed: {
+    releaseTag() {
+      return this.releases.map(r => r.tag_name);
+    },
+    selectedRelease() {
+      return this.releases.find(r => r.tag_name === this.selectedReleaseTag);
+    },
+    selectedReleaseAssets() {
+      if (this.selectedRelease) {
+        return this.selectedRelease.assets;
+      }
+      return [];
+    },
+  },
+  data() {
+    return {
+      state: 'Download',
+
+      selectedReleaseTag: '',
+      releases: [],
+      selectedFiles: [],
+    };
+  },
+  methods: {
+    async dl() {
+      this.state = 'Generating zip file';
+      saveAs(`http://localhost:9000/downloadBundle?ids=${this.selectedFiles.join(',')}`, 'greenworks-binaries.zip');
+      this.state = 'Download';
+    },
+  },
+  async mounted() {
+    const rep = await ky
+      .get('https://api.github.com/repos/ElectronForConstruct/greenworks-prebuilds/releases')
+      .json();
+    console.log(rep);
+    this.releases = rep;
+    this.selectedReleaseTag = this.releases[0].tag_name;
   },
 };
 </script>
--- src_functions/downloadBundle.js
+++ src_functions/downloadBundle.js
@@ -1,3 +1,54 @@
-exports.handler = function (event, context, callback) {
+import got from 'got';
+import JSZIP from 'jszip';
 
+exports.handler = async function (event, context, callback) {
+  const { ids: _ids } = event.queryStringParameters;
+  const ids = _ids.split(',');
+
+  const zip = new JSZIP();
+
+  console.log('ids', ids);
+
+  for (let i = 0; i < ids.length; i += 1) {
+    const id = ids[i];
+    const url = `https://api.github.com/repos/ElectronForConstruct/greenworks-prebuilds/releases/assets/${id}`;
+
+    // eslint-disable-next-line
+    const infos = await got(url, { json: true });
+    // console.log('infos', infos.body);
+
+    // eslint-disable-next-line
+    const stream = got(url, {
+      stream: true,
+      headers: {
+        Accept: 'application/octet-stream',
+      },
+    });
+
+    console.log(`File ${i}`);
+
+    zip.file(`${infos.body.name}.node`, stream);
+  }
+
+  zip
+    .generateAsync({
+      type: 'base64',
+      compression: 'DEFLATE',
+    })
+    .then((zipFile) => {
+      callback(null, {
+        headers: {
+          'Content-Type': 'application/zip, application/octet-stream',
+          'Content-disposition': 'attachment; filename=greenworks-binaries.zip',
+
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        },
+        body: zipFile,
+        isBase64Encoded: true,
+        statusCode: 200,
+      });
+    }, (error) => {
+      callback(error);
+    });
 };
