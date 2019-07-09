f4729f6887291e5255fb836549940af8744676ca
Quentin Goinaud <armaldio@gmail.com>
2019-07-10 00:22:10 +0200

    add modules abi range

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import './plugins/vuetify';
+import AsyncComputed from 'vue-async-computed';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Vue.use(AsyncComputed);
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,149 +1,150 @@
 <template>
-    <div class="home">
-        <v-navigation-drawer clipped fixed app permanent :width="250">
-            <v-list dense>
-                <v-subheader>Release Tag</v-subheader>
+  <div class="home">
+    <v-navigation-drawer clipped fixed app permanent :width="250">
+      <v-list dense>
+        <v-subheader>Release Tag</v-subheader>
 
-                <v-radio-group v-model="selectedReleaseTag"
-                               mandatory
-                               class="full-width"
-                               v-if="releases.length > 0">
-                    <v-list-tile @click="() => {}"
-                                 v-for="release in releases"
-                                 :key="release.tag_name">
-                        <v-list-tile-action>
-                            <v-radio :value="release"></v-radio>
-                        </v-list-tile-action>
+        <v-radio-group v-model="selectedReleaseTag"
+                       mandatory
+                       class="full-width"
+                       v-if="releases.length > 0">
+          <v-list-tile @click="() => {}"
+                       v-for="release in releases"
+                       :key="release.tag_name">
+            <v-list-tile-action>
+              <v-radio :value="release"></v-radio>
+            </v-list-tile-action>
 
-                        <v-list-tile-content @click="selectedReleaseTag = release">
-                            <v-list-tile-title>
-                                {{ release.tag_name }}
-                            </v-list-tile-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </v-radio-group>
-                <v-list-tile @click="() => {}"
-                             v-else>
-                    <v-list-tile-content>
-                        <v-list-tile-title>
-                            Loading releases...
-                        </v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
+            <v-list-tile-content @click="selectedReleaseTag = release">
+              <v-list-tile-title>
+                {{ release.tag_name }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-radio-group>
+        <v-list-tile @click="() => {}"
+                     v-else>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Loading releases...
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
 
-                <v-subheader>OS</v-subheader>
-                <v-list-tile @click="() => {}" v-for="os in oses" :key="os.name">
-                    <v-list-tile-action>
-                        <v-checkbox v-model="os.value"></v-checkbox>
-                    </v-list-tile-action>
+        <v-subheader>OS</v-subheader>
+        <v-list-tile @click="() => {}" v-for="os in oses" :key="os.name">
+          <v-list-tile-action>
+            <v-checkbox v-model="os.value"></v-checkbox>
+          </v-list-tile-action>
 
-                    <v-list-tile-content @click="os.value = !os.value">
-                        <v-list-tile-title>
-                            {{ os.name }}
-                        </v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
+          <v-list-tile-content @click="os.value = !os.value">
+            <v-list-tile-title>
+              {{ os.name }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
 
-                <v-subheader>Arch</v-subheader>
-                <v-list-tile @click="() => {}" v-for="arch in arches" :key="arch.name">
-                    <v-list-tile-action>
-                        <v-checkbox v-model="arch.value"></v-checkbox>
-                    </v-list-tile-action>
+        <v-subheader>Arch</v-subheader>
+        <v-list-tile @click="() => {}" v-for="arch in arches" :key="arch.name">
+          <v-list-tile-action>
+            <v-checkbox v-model="arch.value"></v-checkbox>
+          </v-list-tile-action>
 
-                    <v-list-tile-content @click="arch.value = !arch.value">
-                        <v-list-tile-title>
-                            {{ arch.name }}
-                        </v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
+          <v-list-tile-content @click="arch.value = !arch.value">
+            <v-list-tile-title>
+              {{ arch.name }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
 
-                <v-subheader>Runtime</v-subheader>
-                <v-list-tile @click="() => {}" v-for="runtime in runtimes" :key="runtime.name">
-                    <v-list-tile-action>
-                        <v-checkbox v-model="runtime.value"></v-checkbox>
-                    </v-list-tile-action>
+        <v-subheader>Runtime</v-subheader>
+        <v-list-tile @click="() => {}" v-for="runtime in runtimes" :key="runtime.name">
+          <v-list-tile-action>
+            <v-checkbox v-model="runtime.value"></v-checkbox>
+          </v-list-tile-action>
 
-                    <v-list-tile-content @click="runtime.value = !runtime.value">
-                        <v-list-tile-title>
-                            {{ runtime.name }}
-                        </v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
+          <v-list-tile-content @click="runtime.value = !runtime.value">
+            <v-list-tile-title>
+              {{ runtime.name }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
 
-                <v-subheader>Versions</v-subheader>
-                <v-list-tile @click="() => {}" v-for="version in versions" :key="version.name">
-                    <v-list-tile-action>
-                        <v-checkbox v-model="version.value"></v-checkbox>
-                    </v-list-tile-action>
+        <v-subheader>Versions</v-subheader>
+        <v-list-tile @click="() => {}" v-for="version in versions" :key="version.name">
+          <v-list-tile-action>
+            <v-checkbox v-model="version.value"></v-checkbox>
+          </v-list-tile-action>
 
-                    <v-list-tile-content @click="version.value = !version.value">
-                        <v-list-tile-title>
-                            {{ version.name }}
-                        </v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-            </v-list>
-        </v-navigation-drawer>
+          <v-list-tile-content @click="version.value = !version.value">
+            <v-list-tile-title>
+              {{ version.name }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-btn
-                fixed
-                bottom
-                right
-                fab
-                :disabled="selectedFiles.length === 0 || loadingDialog === true"
-                @click="dl">
-            <v-icon>fas fa-download</v-icon>
-        </v-btn>
+    <v-btn
+      fixed
+      bottom
+      right
+      fab
+      :disabled="selectedFiles.length === 0 || loadingDialog === true"
+      @click="dl">
+      <v-icon>fas fa-download</v-icon>
+    </v-btn>
 
-        <v-layout row wrap>
-            <v-flex xs12 class="pb-2">
-                <v-text-field
-                        v-model="search"
-                        append-icon="fas fa-search"
-                        label="Search"
-                        single-line
-                        hide-details
-                ></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-                <v-data-table v-model="selectedFiles"
-                              :headers="headers"
-                              :items="filteredReleaseAssets"
-                              item-key="name"
-                              select-all
-                              sort-icon="fas fa-chevron-circle-down"
-                              :search="search"
-                              :rows-per-page-items="[
+    <v-layout row wrap>
+      <v-flex xs12 class="pb-2">
+        <v-text-field
+          v-model="search"
+          append-icon="fas fa-search"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs12>
+        <v-data-table v-model="selectedFiles"
+                      :headers="headers"
+                      :items="filteredReleaseAssets"
+                      v-if="filteredReleaseAssets"
+                      item-key="name"
+                      select-all
+                      sort-icon="fas fa-chevron-circle-down"
+                      :search="search"
+                      :rows-per-page-items="[
                               {'text':'$vuetify.dataIterator.rowsPerPageAll','value':-1}
                           ]"
-                              class="elevation-1">
-                    <template v-slot:items="props">
-                        <td>
-                            <v-checkbox v-model="props.selected" primary hide-details></v-checkbox>
-                        </td>
-                        <td class="text-xs-center">{{ props.item.runtime }}</td>
-                        <td class="text-xs-center">{{ props.item.abi }}</td>
-                        <td class="text-xs-center">{{ props.item.os }}</td>
-                        <td class="text-xs-center">{{ props.item.arch }}</td>
-                        <td class="text-xs-center">{{ props.item.versions }}</td>
-                    </template>
-                </v-data-table>
-            </v-flex>
+                      class="elevation-1">
+          <template v-slot:items="props">
+            <td>
+              <v-checkbox v-model="props.selected" primary hide-details></v-checkbox>
+            </td>
+            <td class="text-xs-center">{{ props.item.runtime }}</td>
+            <td class="text-xs-center">{{ props.item.abi }}</td>
+            <td class="text-xs-center">{{ props.item.os }}</td>
+            <td class="text-xs-center">{{ props.item.arch }}</td>
+            <td class="text-xs-center">{{ props.item.versions ? props.item.versions : '' }}</td>
+          </template>
+        </v-data-table>
+      </v-flex>
 
-            <v-dialog v-model="loadingDialog" width="500" persistent>
-                <v-card>
-                    <v-card-text>
-                        Preparing your files...
-                        <v-progress-linear
-                                indeterminate
-                                color="white"
-                                class="mb-0"
-                        ></v-progress-linear>
-                    </v-card-text>
-                </v-card>
-            </v-dialog>
-        </v-layout>
-    </div>
+      <v-dialog v-model="loadingDialog" width="500" persistent>
+        <v-card>
+          <v-card-text>
+            Preparing your files...
+            <v-progress-linear
+              indeterminate
+              color="white"
+              class="mb-0"
+            ></v-progress-linear>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+  </div>
 </template>
 
 <script>
@@ -152,6 +153,26 @@ import ky from 'ky';
 
 const sleep = m => new Promise(r => setTimeout(r, m));
 
+const mapped = async (asset) => {
+  const arr = asset.name.split(/(.*?)-(.*)-(v.*?)-(.*?)-(.*?)\.node/);
+  let range = null;
+  try {
+    const rt = arr[2].replace('node-webkit', 'nw.js');
+    const abi = parseInt(arr[3].replace('v', ''), 10);
+    range = await ky.get(`https://modules-abi.armaldio.now.sh/api?mode=range&runtime=${rt}&abi=${abi}`).json();
+  } catch (e) {
+    console.log('unable to fetch');
+  }
+  return Object.assign({}, asset, {
+    runtime: arr[2],
+    abi: arr[3],
+    os: arr[4],
+    arch: arr[5],
+    // eslint-disable-next-line
+    versions: range ? (range.length === 2 ? `From ${range[0]} to ${range[1]}` : range.join(', ')) : range,
+  });
+};
+
 export default {
   name: 'home',
   filters: {
@@ -159,21 +180,15 @@ export default {
       return value.replace(/(.*?)-(.*)-(v.*?)-(.*?)-(.*?)\.node/, '$2 $3 $4 $5');
     },
   },
-  computed: {
-    selectedRelease() {
-      return this.releases.find(r => r === this.selectedReleaseTag);
-    },
-    filteredReleaseAssets() {
+  asyncComputed: {
+    async filteredReleaseAssets() {
       if (this.selectedRelease) {
-        const assets = this.selectedRelease.assets.map((asset) => {
-          const arr = asset.name.split(/(.*?)-(.*)-(v.*?)-(.*?)-(.*?)\.node/);
-          return Object.assign({}, asset, {
-            runtime: arr[2],
-            abi: arr[3],
-            os: arr[4],
-            arch: arr[5],
-          });
-        });
+        const assets = [];
+        for (let i = 0; i < this.selectedRelease.assets.length; i += 1) {
+          const asset = this.selectedRelease.assets[i];
+          // eslint-disable-next-line
+            assets.push(await mapped(asset));
+        }
         return assets
           .filter(this.filterOs)
           .filter(this.filterArch)
@@ -181,6 +196,11 @@ export default {
           .filter(this.filterVersion);
       }
       return [];
+    },
+  },
+  computed: {
+    selectedRelease() {
+      return this.releases.find(r => r === this.selectedReleaseTag);
     },
   },
   data() {
@@ -342,19 +362,19 @@ export default {
 </script>
 
 <style lang="scss">
-    .full-width {
-        margin-top: 0 !important;
+  .full-width {
+    margin-top: 0 !important;
 
-        > div {
-            width: 100% !important;
-        }
+    > div {
+      width: 100% !important;
     }
+  }
 
-    th {
-        > i {
-            vertical-align: middle;
-            margin-right: 10px;
-        }
+  th {
+    > i {
+      vertical-align: middle;
+      margin-right: 10px;
     }
+  }
 
 </style>
