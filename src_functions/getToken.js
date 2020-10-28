8048ef49fc552f0fe28f2961460879fda593f194
Quentin Goinaud <armaldio@gmail.com>
2020-10-28 22:33:09 +0100

    -

--- src_functions/getToken.js
+++ src_functions/getToken.js
@@ -1,7 +1,7 @@
 import request from 'request';
 import util from 'util';
 
-const isDev = process.env.NODE_ENV === 'production';
+// const isDev = process.env.NODE_ENV === 'production';
 
 const rq = util.promisify(request);
 
@@ -12,8 +12,8 @@ exports.handler = async function (event) {
     url: 'https://github.com/login/oauth/access_token',
     method: 'POST',
     json: {
-      client_id: isDev ? process.env.GH_CLIENT_ID_DEV : process.env.GH_CLIENT_ID,
-      client_secret: isDev ? process.env.GH_CLIENT_SECRET_DEV : process.env.GH_CLIENT_SECRET,
+      client_id: /* isDev ? process.env.GH_CLIENT_ID_DEV :  */process.env.GH_CLIENT_ID,
+      client_secret: /* isDev ? process.env.GH_CLIENT_SECRET_DEV :  */process.env.GH_CLIENT_SECRET,
       code,
     },
   });
