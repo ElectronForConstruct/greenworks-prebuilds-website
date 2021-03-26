bd0c422ef733bc4ef5f9276d8459def8ee16be94
Armaldio <armaldio@gmail.com>
2021-03-26 08:31:02 +0100

    downgrade sass-loader to 10

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-  root         : true,
-  env          : {
+  root: true,
+  env: {
     node: true,
   },
-  extends      : [
+  extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
     'plugin:cypress/recommended',
   ],
-  plugins      : [
+  plugins: [
     'vuetify',
     'cypress',
   ],
-  rules        : {
+  rules: {
     // 'no-console'                     : process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // 'no-debugger'                    : process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prefer-destructuring'           : ['error', {
-      'array' : false,
-      'object': true,
+    'prefer-destructuring': ['error', {
+      array: false,
+      object: true,
     }],
-    'vuetify/no-deprecated-classes'  : 'error',
+    'vuetify/no-deprecated-classes': 'error',
     'vuetify/grid-unknown-attributes': 'error',
-    'vuetify/no-legacy-grid'         : 'error',
-    'no-underscore-dangle'           : 0,
-    'linebreak-style'                : 0,
+    'vuetify/no-legacy-grid': 'error',
+    'no-underscore-dangle': 0,
+    'linebreak-style': 0,
     // 'vue/return-in-computed-property': 0,
   },
   parserOptions: {
